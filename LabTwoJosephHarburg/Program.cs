7db07eb48443d84b84bc7b453701b69741b51547
Joseph Harburg <josephharburg@gmail.com>
2019-10-17 21:26:59 -0400

    Added user inputs and basic calculations

--- LabTwoJosephHarburg/Program.cs
+++ LabTwoJosephHarburg/Program.cs
@@ -6,7 +6,21 @@ namespace LabTwoJosephHarburg
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Calculation Application!\n");
+            Console.WriteLine("Enter the name of the room:");
+            string roomName = Console.ReadLine();
+            Console.WriteLine("Enter a Length:");
+            int userLength = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Width:");
+            int userWidth = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Height:");
+            int userHeight = int.Parse(Console.ReadLine());
+            int area = userLength * userWidth;
+            int perimeter = 2 * (userLength + userWidth);
+            Console.WriteLine($"The area of the {roomName} is {area}\n" +
+                $"The Perimeter of the {roomName} is {perimeter}");
+
+
         }
     }
 }
